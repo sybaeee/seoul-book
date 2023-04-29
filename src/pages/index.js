d5c42a609369c2638f68b7256911c7082a73a0b1
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-30 00:17:54 +0900

    feat: detail 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,11 @@ const Home = () => {
   return (
     <>
       <h1>Main 화면</h1>
+      <Link href="/registerForm">
+        <p>여행지 등록 폼</p>
+      </Link>
       <Link href="/detail">
-        <p>Go to Form page</p>
+        <p>여행지 상세 페이지</p>
       </Link>
     </>
   );
