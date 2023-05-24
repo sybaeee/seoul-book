526d1a0a4cf14374c50935d8ff7692c1312653c7
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-25 00:54:10 +0900

    fix: note 파일명 수정

---
+++ src/components/MarkupNote.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
+
+const MarkupNote = () => {
+  const [content, setContent] = useState("");
+
+  const handleEditorChange = (value) => {
+    setContent(value);
+  };
+
+  const quillStyle = {
+    height: "100%",
+  };
+
+  return (
+    <div
+      style={{
+        width: "800px",
+        height: "500px",
+        backgroundColor: "#fff",
+        borderRadius: "10px",
+      }}
+    >
+      <ReactQuill
+        value={content}
+        onChange={handleEditorChange}
+        style={quillStyle}
+      />
+    </div>
+  );
+};
+
+export default MarkupNote;
