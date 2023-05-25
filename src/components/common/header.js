f326f257e50d805246f22bcc8f79b0ab07964d62
sunghyun <ash5541@naver.com>
2023-05-25 23:39:51 +0900

    '캐러셀구현'

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -4,11 +4,18 @@ import { useState } from "react";
 
 
 const Header = () => {
-
   const router = useRouter()
-  const [isLogin, setIsLogin] = useState(false)
-  const LoginSuccess = () => {
-    setIsLogin(!isLogin)
+  const [isLoginPage, setIsLoginPage] = useState(false)
+
+
+
+  const goLoginPage = () => {
+    router.push('/auth/login')
+    setIsLoginPage(true)
+  }
+
+  const onGoProfilePage = () => {
+    router.push('/profile')
   }
 
   return (
@@ -18,26 +25,26 @@ const Header = () => {
       </LogoWrap>
       <HearderLIstWrap>
         <HearderList>
-          <li onClick={() => { router.push('/') }}>홈</li>
+          <li onClick={() => router.push('/')}>홈</li>
           <li>오늘의 추천</li>
-          <li onClick={() => { router.push('/profile') }}>실시간 예약</li>
+          <li onClick={onGoProfilePage}>실시간 예약</li>
           <li>커뮤니티</li>
-          <li onClick={() => { router.push('/chat') }}>채팅</li>
+          <li onClick={() => router.push('/chat')}>채팅</li>
         </HearderList>
-        {
-          isLogin == false ?
-            <LoginWrap>
-              <LoginButton onClick={LoginSuccess}>로그인</LoginButton>
-              <MembershipButton>회원가입</MembershipButton>
-            </LoginWrap> :
-            <LoginWrap>
-              <LoginButton onClick={() => { router.push('/dashboard') }}>마이페이지</LoginButton>
-              <MembershipButton onClick={LoginSuccess}>로그아웃</MembershipButton>
-            </LoginWrap>
-        }
+        {isLoginPage ? (
+          <LoginWrap>
+            <LoginButton onClick={() => router.push('/dashboard')}>마이페이지</LoginButton>
+            <MembershipButton onClick={() => setIsLoginPage(false)}>로그아웃</MembershipButton>
+          </LoginWrap>
+        ) : (
+          <LoginWrap>
+            <LoginButton onClick={goLoginPage}>로그인</LoginButton>
+            <MembershipButton onClick={() => router.push('/auth/register')}>회원가입</MembershipButton>
+          </LoginWrap>
+        )}
       </HearderLIstWrap>
     </HeaderWrap>
   )
 }
 
-export default Header;
+export default Header
