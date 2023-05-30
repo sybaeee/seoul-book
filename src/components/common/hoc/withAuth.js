6c2547fb7ba4ccba65c904cbf9ec525ca00f94f2
sunghyun <ash5541@naver.com>
2023-05-30 21:36:54 +0900

    'test'

--- src/components/common/hoc/withAuth.js
+++ src/components/common/hoc/withAuth.js
@@ -1,15 +1,15 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 
-export const withAuth = (Component)=>(props)=>{
+export const withAuth = (Component) => (props) => {
 	const router = useRouter()
 
-	useEffect(()=>{
-		if(!localStorage.getItem("accessToken")){
+	useEffect(() => {
+		if (!localStorage.getItem("accessToken")) {
 			alert("로그인을 먼저 해주세요")
 			void router.push("/auth/login");
 		}
-	},[])
+	}, [])
 
 	return <Component {...props} />
 }
